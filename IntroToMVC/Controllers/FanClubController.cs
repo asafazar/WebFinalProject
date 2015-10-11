7a968e389e0454cb846694aa63d583624ee0c716
benoaviram <Beno.Aviram@gmail.com>
2015-10-11 23:41:53 +0300

    Adding Logon With Facebook

--- IntroToMVC/Controllers/FanClubController.cs
+++ IntroToMVC/Controllers/FanClubController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using IntroToMVC.Models;
 using System.Collections;
+using System.IO;
 
 namespace IntroToMVC.Controllers
 {
@@ -24,6 +25,7 @@ namespace IntroToMVC.Controllers
         private FanDBContext db = new FanDBContext();
         private static string message = "OK";
         private static Status status = Status.Off;
+        private static string access_token = "";
 
         public Status getStatus()
         {
@@ -35,7 +37,53 @@ namespace IntroToMVC.Controllers
             ViewBag.HtmlStr = message;
             return View();
         }
+        public ActionResult LogInFaceBook()
+        {
+            string app_id = "838250792939146";
+            string app_secret = "5e6a472a233200ee63876f238adce751";
+            string scope = "publish_actions,manage_pages";
 
+            if (Request["code"] == null)
+            {
+                Response.Redirect(string.Format(
+                    "https://graph.facebook.com/oauth/authorize?client_id={0}&redirect_uri={1}&scope={2}",
+                    app_id, Request.Url.AbsoluteUri, scope));
+            }
+            else
+            {
+                Dictionary<string, string> tokens = new Dictionary<string, string>();
+                string url = string.Format("https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&scope={2}&code={3}&client_secret={4}",
+                    app_id, Request.Url.AbsoluteUri, scope, Request["code"].ToString(), app_secret);
+
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                    string vals = reader.ReadToEnd();
+                    foreach(string token in vals.Split('&'))
+                    {
+                        tokens.Add(token.Substring(0, token.IndexOf("=")),
+                                   token.Substring(token.IndexOf("=") + 1, token.Length - token.IndexOf("=") - 1));
+                    }
+                }
+
+                access_token = tokens["access_token"];
+            }
+
+            if (access_token != "")
+            {
+                status = Status.Logged;
+                this.Session["isAdmin"] = "No";
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                message = "Incorrect details";
+                return RedirectToAction("Login", "FanClub"); 
+            }
+        }
         public ActionResult VerifyUser(string userName, string password)
         {
             var fans = from f in db.Fans
